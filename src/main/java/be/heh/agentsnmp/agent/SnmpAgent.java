d38ad2a3723fff5e7ab2853935f802b1b471cbbe
SimkeAnthony <anthony.simke@std.heh.be>
2023-05-12 17:05:34 +0200

    implémentation agent SNMP complet, erreur 1.3.6.1.6.3.15.1.1.4: usmStatsUnknowEngineId

--- src/main/java/be/heh/agentsnmp/agent/SnmpAgent.java
+++ src/main/java/be/heh/agentsnmp/agent/SnmpAgent.java
@@ -1,6 +1,5 @@
 package be.heh.agentsnmp.agent;
 
-import jdk.jfr.Unsigned;
 import lombok.Getter;
 import lombok.Setter;
 import org.snmp4j.MessageDispatcherImpl;
@@ -38,9 +37,8 @@ public class SnmpAgent extends BaseAgent {
 
     public SnmpAgent(CommandProcessor commandProcessor) throws IOException {
         super(new File("BootCounter.cfg"),new File("AgentConfig.cfg"),commandProcessor);
-        agent.setWorkerPool(ThreadPool.create("requestPool",4));
+        agent.setWorkerPool(ThreadPool.create("requestPool",10));
     }
-
 
     @Override
     protected void registerManagedObjects() {
@@ -50,7 +48,7 @@ public class SnmpAgent extends BaseAgent {
             getAgentppSimulationMib().registerMOs(server,null);
             getSnmp4jHeartbeatMib().registerMOs(server,null);
         } catch (DuplicateRegistrationException e) {
-            e.printStackTrace();
+            System.out.println("Error duplicated MO: "+e.getMessage());
             throw new RuntimeException(e);
         }
     }
@@ -65,57 +63,32 @@ public class SnmpAgent extends BaseAgent {
 
     @Override
     protected void addNotificationTargets(SnmpTargetMIB targetMIB, SnmpNotificationMIB notificationMIB) {
-
         targetMIB.addDefaultTDomains();
-
         /*
-        * SNMP :  version 2c
+        * SNMP : version 3
         */
         targetMIB.addTargetAddress(
-                new OctetString("notificationv2c"),
+                new OctetString("notificationV3"),
                 TransportDomains.transportDomainUdpIpv4,
                 new OctetString(new UdpAddress("127.0.0.1/162").getValue()),
-                200,
-                1,
+                1000,
+                3,
                 new OctetString("notify"),
-                new OctetString("v2c"),
+                new OctetString("v3notify"),
                 StorageType.permanent
         );
 
         targetMIB.addTargetParams(
-                new OctetString("v2c"),
-                MessageProcessingModel.MPv2c,
-                SecurityModel.SECURITY_MODEL_SNMPv2c,
-                new OctetString("cpublic"),
+                new OctetString("v3notify"),
+                MessageProcessingModel.MPv3,
+                SecurityModel.SECURITY_MODEL_USM,
+                new OctetString("v3notify"),
                 SecurityLevel.AUTH_PRIV,
                 StorageType.permanent
         );
 
         /*
-        * SNMP : version 3
-        */
-        targetMIB.addTargetAddress(
-                new OctetString("notificationv3"),
-                TransportDomains.transportDomainUdpIpv4,
-                new OctetString(new UdpAddress("127.0.0.1/1162").getValue()),
-                200,
-                1,
-                new OctetString("notify"),
-                new OctetString("v3notify"),
-                StorageType.permanent
-        );
-
-        targetMIB.addTargetParams(
-                new OctetString("v3"),
-                MessageProcessingModel.MPv3,
-                SecurityModel.SECURITY_MODEL_USM,
-                new OctetString("v3notify"),
-                SecurityLevel.NOAUTH_NOPRIV,
-                StorageType.permanent
-        );
-
-        /*
-        * Default
+        * Notification default
         */
         notificationMIB.addNotifyEntry(
                 new OctetString("default"),
@@ -130,106 +103,6 @@ public class SnmpAgent extends BaseAgent {
 
         //              Group configuration                //
         //-------------------------------------------------//
-        //=================================================//
-        //          All Configuration possible             //
-        //=================================================//
-        /*
-        * SNMP : version 1
-        */
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_SNMPv1,
-                new OctetString("cpublic"),
-                new OctetString("v1v2group"),
-                StorageType.nonVolatile
-        );
-
-        /*
-         * SNMP : version 2c
-         */
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_SNMPv2c,
-                new OctetString("cpublic"),
-                new OctetString("v1v2group"),
-                StorageType.nonVolatile
-        );
-
-        /*
-         * SNMP : version 3
-         */
-
-        // -> SHA256 + DES
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_USM,
-                new OctetString("SHADES"),
-                new OctetString("v3group"),
-                StorageType.nonVolatile
-        );
-        // -> MD5 + DES
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_USM,
-                new OctetString("MD5DES"),
-                new OctetString("v3group"),
-                StorageType.nonVolatile
-        );
-        // -> test
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_USM,
-                new OctetString("TEST"),
-                new OctetString("v3test"),
-                StorageType.nonVolatile
-        );
-        // -> SHA256
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_USM,
-                new OctetString("SHA"),
-                new OctetString("v3restricted"),
-                StorageType.nonVolatile
-        );
-        // -> SHA256 + AES128
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_USM,
-                new OctetString("SHAAES128"),
-                new OctetString("v3group"),
-                StorageType.nonVolatile
-        );
-        // -> SHA256 + AES192
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_USM,
-                new OctetString("SHAAES192"),
-                new OctetString("v3group"),
-                StorageType.nonVolatile
-        );
-        // -> SHA256 + AES256
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_USM,
-                new OctetString("SHAAES256"),
-                new OctetString("v3group"),
-                StorageType.nonVolatile
-        );
-        // -> MD5 + AES128
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_USM,
-                new OctetString("MD5AES128"),
-                new OctetString("v3group"),
-                StorageType.nonVolatile
-        );
-        // -> MD5 + AES192
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_USM,
-                new OctetString("MD5AES192"),
-                new OctetString("v3group"),
-                StorageType.nonVolatile
-        );
-        // -> MD5 + AES256
-        vacmMIB.addGroup(
-                SecurityModel.SECURITY_MODEL_USM,
-                new OctetString("MD5AES256"),
-                new OctetString("v3group"),
-                StorageType.nonVolatile
-        );
-        //=================================================//
-
-
         //=================================================//
         //            Personal configuration               //
         //=================================================//
@@ -249,42 +122,6 @@ public class SnmpAgent extends BaseAgent {
         //             Access Configuration                //
         //-------------------------------------------------//
         //=================================================//
-        //-> group v1v2group : not necessary
-        vacmMIB.addAccess(
-                new OctetString("v1v2group"),
-                new OctetString("public"),
-                SecurityModel.SECURITY_MODEL_ANY,
-                SecurityLevel.NOAUTH_NOPRIV,
-                MutableVACM.VACM_MATCH_EXACT,
-                new OctetString("fullReadView"),
-                new OctetString("fullWriteView"),
-                new OctetString("fullNotifyView"),
-                StorageType.nonVolatile
-        );
-        //-> group v3test : not necessary
-        vacmMIB.addAccess(
-                new OctetString("v3test"),
-                new OctetString(),
-                SecurityModel.SECURITY_MODEL_USM,
-                SecurityLevel.AUTH_PRIV,
-                MutableVACM.VACM_MATCH_EXACT,
-                new OctetString("testReadView"),
-                new OctetString("testWriteView"),
-                new OctetString("testNotifyView"),
-                StorageType.nonVolatile
-        );
-        //-> group v3restricted : not necessary
-        vacmMIB.addAccess(
-                new OctetString("v3restricted"),
-                new OctetString(),
-                SecurityModel.SECURITY_MODEL_USM,
-                SecurityLevel.NOAUTH_NOPRIV,
-                MutableVACM.VACM_MATCH_EXACT,
-                new OctetString("restrictedReadView"),
-                new OctetString("restrictedWriteView"),
-                new OctetString("restrictedNotifyView"),
-                StorageType.nonVolatile
-        );
         //-> group v3group
         vacmMIB.addAccess(
                 new OctetString("v3group"),
@@ -302,78 +139,6 @@ public class SnmpAgent extends BaseAgent {
         //                   Add Views                     //
         //-------------------------------------------------//
         //=================================================//
-        /*
-        * TestView : not necessary
-        */
-        //-> read included
-        vacmMIB.addViewTreeFamily(
-                new OctetString("testReadView"),
-                new OID("1.3.6.1.2"),
-                new OctetString(),
-                VacmMIB.vacmViewIncluded,
-                StorageType.nonVolatile
-        );
-        //-> read excluded
-        vacmMIB.addViewTreeFamily(
-                new OctetString("testReadView"),
-                new OID("1.3.6.1.2.1.1"),
-                new OctetString(),
-                VacmMIB.vacmViewExcluded,
-                StorageType.nonVolatile
-        );
-        //-> write
-        vacmMIB.addViewTreeFamily(
-                new OctetString("testWriteView"),
-                new OID("1.3.6.1.2.1"),
-                new OctetString(),
-                VacmMIB.vacmViewIncluded,
-                StorageType.nonVolatile
-        );
-        //-> notify
-        vacmMIB.addViewTreeFamily(
-                new OctetString("testNotifyView"),
-                new OID("1.3.6.1.2"),
-                new OctetString(),
-                VacmMIB.vacmViewIncluded,
-                StorageType.nonVolatile
-        );
-
-        /*
-        * RestrictedView : not necessary
-        */
-        //-> read
-        vacmMIB.addViewTreeFamily(
-                new OctetString("restrictedReadView"),
-                new OID("1.3.6.1.2"),
-                new OctetString(),
-                VacmMIB.vacmViewIncluded,
-                StorageType.nonVolatile
-        );
-        //-> write
-        vacmMIB.addViewTreeFamily(
-                new OctetString("restrictedWriteView"),
-                new OID("1.3.6.1.2.1"),
-                new OctetString(),
-                VacmMIB.vacmViewIncluded,
-                StorageType.nonVolatile
-        );
-        //-> notify 1
-        vacmMIB.addViewTreeFamily(
-                new OctetString("restrictedNotifyView"),
-                new OID("1.3.6.1.2"),
-                new OctetString(),
-                VacmMIB.vacmViewIncluded,
-                StorageType.nonVolatile
-        );
-        //-> notify 2
-        vacmMIB.addViewTreeFamily(
-                new OctetString("restrictedNotifyView"),
-                new OID("1.3.6.1.6.3.1"),
-                new OctetString(),
-                VacmMIB.vacmViewIncluded,
-                StorageType.nonVolatile
-        );
-
         /*
         * fullView
         */
@@ -420,11 +185,12 @@ public class SnmpAgent extends BaseAgent {
                 new OctetString("Silver-Major-Knight-16")
         );
         usm.addUser(user.getSecurityName(),usm.getLocalEngineID(),user);
+        this.usm = usm;
     }
     @Override
     protected void addCommunities(SnmpCommunityMIB communityMIB) {
         Variable[] com2sec = new Variable[]{
-                new OctetString(),                      //community name
+                new OctetString("public"),    //community name
                 new OctetString("anthony"),   //security name
                 getAgent().getContextEngineID(),       //local engine ID
                 new OctetString("public"),    //default context name
@@ -441,18 +207,20 @@ public class SnmpAgent extends BaseAgent {
         //snmpCommunityMIB.setSourceAddressFiltering(true);
     }
 
+    @Override
     protected void registerSnmpMIBs(){
         setSnmp4jHeartbeatMib(
                 new Snmp4jHeartbeatMib(
                         super.getNotificationOriginator(),
                         new OctetString(),
-                        super.snmpv2MIB.getSysUpTime()
+                        super.getSnmpv2MIB().getSysUpTime()
                 )
         );
         setAgentppSimulationMib(new AgentppSimulationMib());
         super.registerSnmpMIBs();
     }
 
+    @Override
     protected void initTransportMappings() throws IOException {
         transportMappings = new TransportMapping[2];
         Address addr = GenericAddress.parse(getAddress());
@@ -460,14 +228,13 @@ public class SnmpAgent extends BaseAgent {
         transportMappings[0] = tm;
         transportMappings[1] = new DefaultTcpTransportMapping(new TcpAddress(getAddress()));
     }
+    @Override
     protected void initMessageDispatcher(){
         this.dispatcher = new MessageDispatcherImpl();
         this.mpv3 = new MPv3(this.agent.getContextEngineID().getValue());
         this.usm = new USM(SecurityProtocols.getInstance(),this.agent.getContextEngineID(),this.updateEngineBoots());
         SecurityModels.getInstance().addSecurityModel(this.usm);
         SecurityProtocols.getInstance().addDefaultProtocols();
-        this.dispatcher.addMessageProcessingModel(new MPv1());
-        this.dispatcher.addMessageProcessingModel(new MPv2c());
         this.dispatcher.addMessageProcessingModel(new MPv3());
         this.initSnmpSession();
     }
@@ -508,7 +275,7 @@ public class SnmpAgent extends BaseAgent {
 
         columns[c++] = new MOColumn(c,SMIConstants.SYNTAX_TIMETICKS,MOAccessImpl.ACCESS_READ_ONLY); //ifCounterDiscontinuityTime
 
-        DefaultMOTable ifXTable = new DefaultMOTable(new OID(".1.3.6.1.2.1.31.1.1.1"),indexDef,columns);
+        DefaultMOTable ifXTable = new DefaultMOTable(new OID("1.3.6.1.2.1.31.1.1.1"),indexDef,columns);
         MOMutableTableModel model = (MOMutableTableModel) ifXTable.getModel();
         Variable[] rowValues1 = new Variable[]{
                 new OctetString("Ethernet-0"),
@@ -659,12 +426,23 @@ public class SnmpAgent extends BaseAgent {
 
     public void start(String ip, int port) throws IOException{
         if(port>0 && port<=65535){
+            System.out.println("set address : "+ip+"/"+port);
             setAddress(ip+"/"+port);
+            System.out.println("start initialization...");
             init();
+            System.out.println("add shutdown hook");
             addShutdownHook();
+            System.out.println("load config : replace and create");
             loadConfig(ImportMode.REPLACE_CREATE);
+            System.out.println("check configuration: ");
+            System.out.println("localEngineId: "+getUsm().getLocalEngineID());
+            System.out.println("user: "+getUsm().getUser(getUsm().getLocalEngineID(),new OctetString("anthony")));
+            System.out.println("context: "+agent.getContextEngineID());
+            System.out.println("add context to server");
             getServer().addContext(new OctetString("public"));
+            System.out.println("finish initialization...");
             finishInit();
+            System.out.println("start the SNMP agent...");
             run();
             sendColdStartNotification();
         }else{
